dba0b8bca1a4b13257f70829694118e043dcfb07
semyonf1l1pp0v <syomchik13@gmail.com>
2023-06-29 00:36:17 +0300

    functions added for beast characteristics

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import csv
 from config import headers  # my "user agent" and "accept"
 
-URL = "https://vedmak.fandom.com/wiki/Ignis_Fatuus"
+URL = "https://vedmak.fandom.com/wiki/%D0%A1%D0%B0%D0%BB%D1%8C%D0%BC%D0%B0"
 
 
 proxy_tor = "socks5://127.0.0.1:" + str(random.randint(9052, 9139))
@@ -17,11 +17,44 @@ with open("index.html", "r") as file:
     src = file.read()
 
 soup = BeautifulSoup(src, "lxml")
-beast_class = soup.find("div", {'data-source': "Класс"}).find("a").get("title")
-beast_variation = soup.find("div", {'data-source': "Вид"}).find("a").get("title")
-beast_type = soup.find("div", {'data-source': "Тип"}).find("div").text
-beast_location = soup.find("div", {'data-source': "Местонахождение"}).find("div").text
-beast_tactic = soup.find("div", {'data-source': "Тактика"}).find("div").text
-# beast_weakness = soup.find("div", {'data-source': "Уязвимость"}).find("div").find_all("a")
 
+
+def parse_beast_class(soup_obj):
+    beast_class = soup_obj.find("div", {'data-source': "Класс"}).find("a").get("title")
+    return beast_class
+
+
+def parse_beast_variation(soup_obj):
+    beast_variation = soup_obj.find("div", {'data-source': "Вид"}).find("a").get("title")
+    return beast_variation
+
+
+def parse_beast_type(soup_obj):
+    beast_type = soup_obj.find("div", {'data-source': "Тип"}).find("div").text
+    return beast_type
+
+
+def parse_beast_location(soup_obj):
+    beast_location = soup_obj.find("div", {'data-source': "Местонахождение"}).find("div").text
+    return beast_location
+
+
+def parse_beast_tactic(soup_obj):
+    try:
+        beast_tactic = soup_obj.find("div", {'data-source': "Тактика"}).find("div").text
+        return beast_tactic
+    except AttributeError:
+        return "Нет данных"
+# there may be no tactic (exception AttributeError)
+
+
+def parse_beast_weakness(soup_obj):
+    res = []
+    beast_weakness = soup_obj.find("div", {'data-source': "Уязвимость"}).find("div").find_all("a")
+    for weak in beast_weakness:
+        res.append(weak.text)
+    return res
+
+
+print(parse_beast_weakness(soup))
 
